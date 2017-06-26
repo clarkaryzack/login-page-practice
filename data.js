ad60bb2c234e5e355865db41a857e1b04f95ce90
clarkaryzack <clarkaryzack@gmail.com>
2017-06-26 14:02:27 -0400

    Initial commit.

---
+++ data.js
@@ -0,0 +1,23 @@
+module.exports = {
+    users: [{
+            "username": "clarkaryzack",
+						"password": "ZacksPassword"
+					},
+					{
+            "username": "robertcodes",
+						"password": "RobertsPassword"
+					},
+					{
+            "username": "samwise",
+						"password": "SamsPassword"
+					},
+					{
+            "username": "lothlorien",
+						"password": "LoriensPassword"
+					},
+					{
+            "username": "AllAboutThatBass",
+						"password": "MichaelsPassword"
+					},
+  ]
+}
